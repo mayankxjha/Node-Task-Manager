01d722ce351ca6fea08eadc0b908cddd7ccb9502
Mayank Jha <raijin@cuphead.local>
2023-03-08 16:14:05 +0530

    minor fixes needed

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const express = require('express')
+const express = require("express");
+// require('dotenv').config();
 const index = express();
-const tasks = require('./routes/tasks')
-const connectDB = require('./db/connect')
-const dotenv = require("dotenv")
-dotenv.config()
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config();
 
-index.use(express.static('./public'))
-console.log('Task Manager App')
+index.use(express.static(path.join(__dirname, "public")));
+console.log("Task Manager App");
 // app.use(express.urlencoded({extended: false}))
-index.use(express.json())
+index.use(express.json());
+// index.use('/',require('./routes/tasks'))
+index.use("/api/v1/tasks", tasks);
 
-index.use('/api/v1/tasks', tasks)
-
-const port = process.env.PORT || 3000
-const start = async()=>{
+const port = process.env.PORT || 3000;
+const start = async () => {
     await connectDB(process.env.MONGO_URI)
-        .then(() => {console.log('CONNECTED TO DB....')
+        .then(() => {
+            console.log("CONNECTED TO DB....");
         })
-        .catch(error => console.log(error))
-}
-start().then(()=>{
+        .catch((error) => console.log(error));
+};
+start().then(() => {
     index.listen(port, () => {
-        console.log('Server is listening at port 3000')
-    })
-})
+        console.log("Server is listening at port 3000");
+    });
+});
